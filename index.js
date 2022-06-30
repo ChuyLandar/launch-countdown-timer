13839deed24dfb0db07fffaee53ac60d53683637
Chuy Landar <66813976+ChuyLandar@users.noreply.github.com>
2022-06-30 14:58:23 -0500

    feat: add timer functionality

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-fourteenDays = 8.64e7 * 14;
-//const goalDate = new Date("7-1-22").getTime();
-let today = new Date().getTime();
-const goalDate = today + fourteenDays;
+// const timerDays = document.querySelector(".timer-days");
+// const timerHours = document.querySelector(".timer-hours");
+// const timerMinutes = document.querySelector(".timer-minutes");
+// const timerSeconds = document.querySelector(".timer-seconds");
 
-/*
-setInterval(()=>{
-    let today = new Date().getTime();
-    const releaseDate = goalDate - today;
 
-    const d = Math.floor(releaseDate / 8.64e7); 
-    const h = Math.floor(releaseDate % 8.64e7 / 3.6e6); 
-    const m = Math.floor(releaseDate % 3.6e6 / 6e4); 
-    const s = Math.floor(releaseDate % 6e4 / 1e3);
+// fourteenDays = 8.64e7 * 14;
+// let today = new Date().getTime();
+// const goalDate = today + fourteenDays;
 
-    const legend = `Release date: ${d} day, ${h} hours, ${m} minutes and  ${s} seconds`;
-    console.log(legend);
-}, 10000);
-*/
+
+// setInterval(()=>{
+//     let today = new Date().getTime();
+//     const releaseDate = goalDate - today;
+
+//     const d = Math.floor(releaseDate / 8.64e7); 
+//     const h = Math.floor(releaseDate % 8.64e7 / 3.6e6); 
+//     const m = Math.floor(releaseDate % 3.6e6 / 6e4); 
+//     const s = Math.floor(releaseDate % 6e4 / 1e3);
+
+//     timerDays.textContent = d;
+//     timerHours.textContent = h;
+//     timerMinutes.textContent = m;
+//     timerSeconds.textContent = s;
+// }, 1000);
