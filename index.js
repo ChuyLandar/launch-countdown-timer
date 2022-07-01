e3e566cece1238935a153fd6b27145d5605f4db6
Chuy Landar <66813976+ChuyLandar@users.noreply.github.com>
2022-07-01 15:22:49 -0500

    feat: add flip functionality

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-// const timerDays = document.querySelector(".timer-days");
-// const timerHours = document.querySelector(".timer-hours");
-// const timerMinutes = document.querySelector(".timer-minutes");
-// const timerSeconds = document.querySelector(".timer-seconds");
+const timerDays = document.querySelector(".timer-days");
+const timerHours = document.querySelector(".timer-hours");
+const timerMinutes = document.querySelector(".timer-minutes");
+const timerSeconds = document.querySelector(".timer-seconds");
 
 
-// fourteenDays = 8.64e7 * 14;
-// let today = new Date().getTime();
-// const goalDate = today + fourteenDays;
+timerDays.addEventListener("animationend", () => {
+    timerDays.classList.remove("flip");
+});
+timerHours.addEventListener("animationend", () => {
+    timerHours.classList.remove("flip");
+});
+timerMinutes.addEventListener("animationend", () => {
+    timerMinutes.classList.remove("flip");
+});
+timerSeconds.addEventListener("animationend", () => {
+    timerSeconds.classList.remove("flip");
+});
 
 
-// setInterval(()=>{
-//     let today = new Date().getTime();
-//     const releaseDate = goalDate - today;
+fourteenDays = 8.64e7 * 14;
+let today = new Date().getTime();
+const goalDate = today + fourteenDays;
 
-//     const d = Math.floor(releaseDate / 8.64e7); 
-//     const h = Math.floor(releaseDate % 8.64e7 / 3.6e6); 
-//     const m = Math.floor(releaseDate % 3.6e6 / 6e4); 
-//     const s = Math.floor(releaseDate % 6e4 / 1e3);
 
-//     timerDays.textContent = d;
-//     timerHours.textContent = h;
-//     timerMinutes.textContent = m;
-//     timerSeconds.textContent = s;
-// }, 1000);
+setInterval(() => {
+    let today = new Date().getTime();
+    const releaseDate = goalDate - today;
+
+    const d = Math.floor(releaseDate / 8.64e7);
+    const h = Math.floor(releaseDate % 8.64e7 / 3.6e6);
+    const m = Math.floor(releaseDate % 3.6e6 / 6e4);
+    const s = Math.floor(releaseDate % 6e4 / 1e3);
+
+    if (timerDays.textContent != d) {
+        timerDays.classList.add("flip");
+    }
+    if (timerHours.textContent != h) {
+        timerHours.classList.add("flip");
+    }
+    if (timerMinutes.textContent != m) {
+        timerMinutes.classList.add("flip");
+    }
+    if (timerSeconds.textContent != s) {
+        timerSeconds.classList.add("flip");
+    }
+
+    timerDays.textContent = d;
+    timerHours.textContent = h;
+    timerMinutes.textContent = m;
+    timerSeconds.textContent = s;
+
+}, 1000);
